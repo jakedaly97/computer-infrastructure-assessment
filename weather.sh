9a17fb573a2ef9dc91325fd1037124ac8d322bca
jakedaly97 <156820604+jakedaly97@users.noreply.github.com>
2024-11-08 15:30:32 +0000

    Task 7

---
+++ weather.sh
@@ -0,0 +1,7 @@
+#! bin/bash
+
+date
+echo "Downloading weather data"
+weget -O data/weather/`date +"%Y%m%d_%H%M%S_weather.json"` https://prodapi.metweb.ie/observations/athenry/today
+echo "Weather data downloaded"
+date
